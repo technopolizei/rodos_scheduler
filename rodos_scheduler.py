b6b890037ebf0aac7f3e60f4334b31dc7303317d
Leo Sas <technopolizei@gmail.com>
2025-03-06 10:08:18 +0300

    v1.3

--- rodos_scheduler.py
+++ rodos_scheduler.py
@@ -5,23 +5,23 @@ from datetime import datetime
 
 # Список URL-адресов для запросов
 URLS = [
-    "http://192.168.60.11/protect/rb0n.cgi",
-    "http://192.168.60.11/protect/rb1n.cgi",
-    "http://192.168.60.11/protect/rb2n.cgi",
-    "http://192.168.60.11/protect/rb3n.cgi",
-    "http://192.168.60.11/protect/rb4n.cgi",
-    "http://192.168.60.11/protect/rb5n.cgi",
-    "http://192.168.60.11/protect/rb6n.cgi",
-    "http://192.168.60.11/protect/rb7n.cgi"
+    "http://admin:admin@192.168.60.11/protect/rb0n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb1n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb2n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb3n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb4n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb5n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb6n.cgi",
+    "http://admin:admin@192.168.60.11/protect/rb7n.cgi"
 ]
 
 def send_get_requests():
-    today = datetime.today().weekday()
-    if today >= 5:  # Выходные (5=Сб, 6=Вс)
-        print(f"[{datetime.now()}] Выходной день — запросы не отправлены")
-        return
+    # today = datetime.today().weekday()
+    # if today >= 5:  # Выходные (5=Сб, 6=Вс)
+    #     print(f"[{datetime.now()}] Выходной день — запросы не отправлены")
+    #     return
 
-    print(f"[{datetime.now()}] Начало отправки 8 запросов...")
+    # print(f"[{datetime.now()}] Начало отправки 8 запросов...")
     
     for idx, url in enumerate(URLS, 1):
         try:
@@ -34,9 +34,11 @@ def send_get_requests():
     print(f"[{datetime.now()}] Все запросы выполнены\n")
 
 # Настройка расписания
-schedule.every().day.at("08:00").do(send_get_requests)
+# schedule.every().day.at("08:00").do(send_get_requests)
 
-print("Скрипт запущен. Ожидание запросов по расписанию...")
-while True:
-    schedule.run_pending()
-    time.sleep(60)  # Проверка расписания каждую минуту 
+send_get_requests()
+
+# print("Скрипт запущен. Ожидание запросов по расписанию...")
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)  # Проверка расписания каждую минуту 
